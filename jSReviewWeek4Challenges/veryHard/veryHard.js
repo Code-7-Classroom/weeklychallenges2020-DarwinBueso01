54dd4eafe2c30905b2063a7c0ba03aee85b39695
Darwin Bueso <darwingaldamez5@gmail.com>
2020-04-13 15:24:13 -0400

    PushingJsReviewWeek4Challenges

--- jSReviewWeek4Challenges/veryHard/veryHard.js
+++ jSReviewWeek4Challenges/veryHard/veryHard.js
@@ -1,7 +1,9 @@
+var amount = prompt('Type the number of coins you want change for!')
+
 function coinsChange(coinsArray, amount) {
     coinsArray.sort((a, b) => a - b).reverse();
     var count = 0;
-    for (var i = 0; i <= coinsArray.length; i++) {
+    for (var i = 0; i < coinsArray.length; i++) {
         while (amount >= coinsArray[i]) {
             amount -= coinsArray[i];
             count++
@@ -13,6 +15,6 @@ function coinsChange(coinsArray, amount) {
     return count;
 }
 
-var amount = prompt('Type the number of coins you want change for!')
-
 console.log(coinsChange([1, 2, 5, 10, 25], amount));
+
+
